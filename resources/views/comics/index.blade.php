d0b9d5584fba0ec8462635e46d7a42f8db747ee8
casalaspro <casalaspro.alessandro@gmail.com>
2024-05-29 17:29:41 +0200

    ADD - cards to index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,5 +3,22 @@
 @section('content')
 
 <main>
-  <h3>comic.index</h3>
+  <div class="container">
+    <div class="row">
+
+      @foreach ($comics as $comic)
+      <div class="col-3">
+        <div class="card card-comic">
+          <img src="{{ $comic->thumb }}" class="card-img-top comic-cover" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{ $comic->title }}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">More Infos</a>
+          </div>
+        </div>
+      </div>
+      @endforeach
+
+    </div>
+  </div>
 </main>
